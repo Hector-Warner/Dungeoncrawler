f561e225379a3d1dd44606d05e5d96faeb9df9c0
Hector Warner <hxw363@student.bham.ac.uk>
2026-04-29 20:41:08 +0100

    Fixed Chunk Generation

--- Assets/Scripts/TileGenerationScript.cs
+++ Assets/Scripts/TileGenerationScript.cs
@@ -15,46 +15,19 @@ public class TileGenerationScript : MonoBehaviour
     public int chunkSize = 16;
     public Grid tileMapPrefab;
     public PlayerController playerController;
-    private Dictionary<Vector2, Tilemap> loadedChunks = new Dictionary<Vector2, Tilemap>();
+    int counter = 0;
+    private Dictionary<Vector2Int, Grid> loadedChunks = new Dictionary<Vector2Int, Grid>();
 
-
-    public class Chunk : MonoBehaviour
-    {
-        public Vector2Int chunkCoords;
-        public Tilemap chunkTileMap;
-
-        public void Initialisation(int width, int height, Tile[] tiles, Tile[] walls)
-        {
-            chunkTileMap = GetComponentInChildren<Tilemap>();
-
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    if (x == width - 1 || y == height - 1 || x == 0 || y == 0)
-                    {
-                        int tileNum = Random.Range(0, walls.Length);
-                        chunkTileMap.SetTile(new Vector3Int(chunkCoords.x * width, chunkCoords.y * height), walls[tileNum]);
-                    }
-                    else
-                    {
-                        int tileNum = Random.Range(0, tiles.Length);
-
-                        chunkTileMap.SetTile(new Vector3Int(chunkCoords.x * width, chunkCoords.y * height), tiles[tileNum]);
-                    }
-                }
-            }
-        }
-    }
     void Start()
     {
     }
 
-    public void createChunk(Vector2 chunkStart)
+    public void createChunk(Vector2Int chunkStart)
     {
         Grid newChunk = Instantiate(tileMapPrefab);
         Tilemap newTilemap = newChunk.GetComponentInChildren<Tilemap>();
-        newChunk.transform.position = new Vector2(chunkStart.x*chunkSize, chunkStart.y*chunkSize);
+        loadedChunks.Add(chunkStart, newChunk);
+        newChunk.transform.position = new Vector3Int(chunkStart.x*chunkSize, chunkStart.y*chunkSize);
 
         for (int x = 0; x < chunkSize; x++)
         {
@@ -63,7 +36,6 @@ public class TileGenerationScript : MonoBehaviour
                 if (x == chunkSize - 1 || y == chunkSize - 1 || x == 0 || y == 0)
                 {
                     int tileNum = Random.Range(0, walls.Length);
-                    Debug.Log("x: " + x + " y: " + y);
                     newTilemap.SetTile(new Vector3Int(x, y), walls[tileNum]);
                 }
                 else
@@ -74,22 +46,22 @@ public class TileGenerationScript : MonoBehaviour
                 }
             }
         }
-        loadedChunks.Add(chunkStart, newTilemap);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 currentChunk = new Vector2(Mathf.FloorToInt(playerController.transform.position.x / chunkSize), Mathf.FloorToInt(playerController.transform.position.y / chunkSize));
+        Vector2Int currentChunk = new Vector2Int(Mathf.FloorToInt(playerController.transform.position.x / chunkSize), Mathf.FloorToInt(playerController.transform.position.y / chunkSize));
         for (int x = -1; x <= 1; x++)
         {
-            for (int y = -1; y <= 1; y++) { 
-                Vector2 targetCoord = new Vector2(currentChunk.x + x, currentChunk.y + y);
+            for (int y = -1; y <= 1; y++)
+            {
+                Vector2Int targetCoord = new Vector2Int(currentChunk.x + x, currentChunk.y + y);
                 if (!loadedChunks.ContainsKey(targetCoord))
                 {
                     createChunk(targetCoord);
                 }
-            } 
+            }
         }
     }
 }
